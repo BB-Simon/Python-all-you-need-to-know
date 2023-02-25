040bc5552579221ff22de65661bde33ceb2821ef
BB-Simon <simon.chowdery@gmail.com>
2023-02-25 19:52:33 +0200

    While loop

--- while_loop.py
+++ while_loop.py
@@ -1,11 +1,17 @@
-# num = [1, 2, 3, 4, 5, 6]
+# num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 # a = 0
 # while a < len(num):
-#     print(f'#{str(a + 1).zfill(2)} => {num[a]}')
+#     print(f'#{str(a + 1).zfill(4)} => {num[a]}')
 #     a +=1
 # else:
 #     print('Loop has done')
-
+# name = 'Simon'
+# i = 0
+# while i < len(name):
+#     print(f'You are: {name[i].upper()}')
+#     i+=1
+# else:
+#     print('Loop has done')
 # -----------------------------
 # ----------Bookmarks----------
 # -----------------------------
@@ -14,7 +20,7 @@
 # maxbookmarks = 5
 # while maxbookmarks > 0:
 #     web = input('Mark Your Favourit Website?').strip().lower()
-#     bookmarks.append(f'https://{web}')
+#     bookmarks.append(f'https://{web}.com')
 #     maxbookmarks -= 1
 #     print(f'Website added, {maxbookmarks} places left')
 #     print(bookmarks)
@@ -33,34 +39,35 @@
 # ----------Password Guess----------
 # -----------------------------
 
-# tries = 2
+# tries = 5
 # main_password = 'simon123'
 # input_password = input('Enter Your Password: ')
 # while input_password != main_password:
 #     tries -= 1
-#     print(
-#         f"Wrong Password, { 'Last Chance' if tries == 0 else f'{tries} Chances' } Left")
-#     input_password = input('Enter Your Password: ')
-
-#     if tries == 0:
+#     if tries > 0:
+#         print(
+#             f"Wrong Password, { 'Last Chance' if tries == 1 else f'{tries} Chances Left' }")
+#         input_password = input('Enter Your Password: ')
+#     else:
 #         print('Oops...! All Chances Are Finished')
 #         break
 # else:
 #     print('It\'s Done...! Happy Codding')
 
 
-# tries = 3
-# my_havites = ['coding', 'eating', 'running']
-# your_input = input('tell me on of my fav if you\'re my best freind: ')
+tries = 3
+my_havites = ['coding', 'eating', 'running']
+your_input = input('tell me one of my favourite havite if you\'re my best freind: ')
 
-# while your_input not in my_havites:
-#     tries -= 1
-#     print(f"Oops...!,{'Last chance' if tries == 0 else f'{tries} chances'} Left")
-#     your_input = input('tell me on of my fav if you\'re my best freind: ')
+while your_input not in my_havites:
+    tries -= 1
+    if tries > 0:
+        print(
+            f"Oops...! {'Last chance' if tries == 1 else f'{tries} chances left'}")
+        your_input = input('Try again: ')
 
-#     if tries == 0:
-#         print('Oops! all chances are finished, come back letter')
-#         break
-# else:
-#     print('it\'s awsome, you\'re my best friend')
-
+    if tries == 0:
+        print('Oops! all chances are finished, come back letter')
+        break
+else:
+    print('it\'s awsome, you\'re my best friend')
